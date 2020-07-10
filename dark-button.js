f5c36f55b6cefc93263723e79c44b99bddef0e94
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-07-10 20:34:38 -0300

    version terminada

---
+++ dark-button.js
@@ -0,0 +1,37 @@
+
+// Toggle Status
+let darkModeStatus = false;
+// Shorthand of elements
+const body = document.body;
+const divisor = document.querySelectorAll('.divisor');
+const card = document.querySelectorAll('.card');
+const mainNumber = document.querySelectorAll('.main-number');
+const subNumber = document.querySelectorAll('.sub-number');
+const titles = document.querySelectorAll('.titles');
+const overTitle = document.querySelectorAll('.overview-title');
+const followerText = document.querySelectorAll('.follower-text');
+const accountName = document.querySelectorAll('.account-name');
+const accountSub = document.querySelectorAll('.account-subtitle');
+
+// ARRAY OF ELEMENTS
+const elementsArr = [divisor,
+                    card, 
+                    mainNumber, 
+                    subNumber, 
+                    titles, 
+                    overTitle, 
+                    followerText,
+                    accountName,
+                    accountSub];
+// Function
+const darkMode = () => {
+    darkModeStatus = !darkModeStatus;
+    body.classList.toggle('dark')
+    elementsArr.forEach(element => {
+        element.forEach(elementIn => {
+            elementIn.classList.toggle('dark')
+        })
+    })
+}
+
+console.log(card);
